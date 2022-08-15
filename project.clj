0e5d68259a1f58cd4e0d1d4578e3f75edc9fe78a
shiunko <shiunko@hotmail.com>
2022-08-15 21:09:11 +0800

    add project group

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rcon "0.1.0"
+(defproject net.clojars.shiunko/rcon "0.1.0"
   :description "use to connect minecraft cron"
   :url "https://www.zthc.net"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
